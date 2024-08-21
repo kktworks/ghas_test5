31d3759161905f18d873db6a80625128ce2aee32
kikuta501 <hi-kikuta@systems-inc.co.jp>
2024-08-21 11:44:40 +0900

    delete secret key

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,8 +6,6 @@ import java.util.zip.ZipEntry;
 
 public class App {
     public static void main(String[] args) {
-        final String ACCESS_KEY = "AKIAVI4WSBYRED46IP27";
-        final String SECRET_KEY = "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";
     }
 
     public void writeZipEntry(ZipEntry entry, File destinationDir) throws Exception {
